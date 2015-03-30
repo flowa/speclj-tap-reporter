21d29a23d585f1edf9f27e4afb4530434b69250f
Juha Heimonen <heimojuh@gmail.com>
2015-03-30 22:31:15 +0300

    Adds tap header printing
    Tap header is used to see if tap results miss any tests.
    Again, clojure.test.tap already has the sufficient
    method.
    
    It's pretty unkown how nested sets are handled. That calls
    for some testing I believe.

--- src/speclj/report/tap.clj
+++ src/speclj/report/tap.clj
@@ -1,7 +1,6 @@
 (ns speclj.report.tap
   (:require [speclj.reporting]
-            [clojure.test.tap :as tap])
-  (:import [speclj.results PassResult FailResult PendingResult]))
+            [clojure.test.tap :as tap]))
 
 (defn- log [stuff]
   (print stuff)
@@ -10,10 +9,15 @@
 (defn- get-name-from-result [result]
   (.name (.characteristic result)))
 
+(defn- get-number-of-tests-from-description [description]
+  (let [descriptions (.charcteristics description)]
+  (count @descriptions)))
+
 (deftype TapReporter [passes fails results]
   speclj.reporting/Reporter
   (report-message [this message])
-  (report-description [this description])
+  (report-description [this description]
+    (tap/print-tap-plan (get-number-of-tests-from-description description)))
   (report-pass [this result]
     (let [msg (get-name-from-result result)]
       (tap/print-tap-pass msg)))
