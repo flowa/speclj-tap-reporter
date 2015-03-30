a6ee42554719a0408bc8ddb347bade3c6437eeb7
Juha Heimonen <heimojuh@gmail.com>
2015-03-31 00:09:01 +0300

    Changes name of reporter to reporter
    This is not a runner. Too little sleep-

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject speclj-tap-runner "0.1.0-SNAPSHOT"
+(defproject speclj-tap-reporter "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
