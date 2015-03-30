982998710b06e6a989a12090a5afc960c4113775
Juha Heimonen <heimojuh@gmail.com>
2015-03-31 00:10:00 +0300

    Adds test for tap plan. We get 1..5 printed ok
    But the plan printing will have to be changed at some point.
    Most likely we need to do the plan printing at the end
    of the sets.

--- spec/speclj/report/tap_spec.clj
+++ spec/speclj/report/tap_spec.clj
@@ -1,9 +1,24 @@
 (ns speclj-tap-runner.core-spec
   (:require [speclj.core :refer :all]
-            [speclj.report.tap :as tap]))
+            [speclj.report.tap :as tap])
+  (:import  [speclj.components Characteristic Description]))
 
-(describe "a test"
-          (it "FIXME, I fail."
-              (should= 0 1))
-          (it "DO NOT FIXME, I pass"
-              (should= 1 1)))
+(defn- make-characteristics [x]
+  (atom (take x (repeat (Characteristic. "foo" "" "")))))
+
+(defn- generate-description-with-x-charcteristics [x]
+  (let [characteristics (make-characteristics x)]
+    (Description. "foo" "" "" "" characteristics "" "" "" "" "" "" "" "" "")))
+
+(describe "Tap Plan is printed correctly"
+
+          (around [it]
+                  (with-out-str (it)))
+
+          (with reporter (tap/new-tap-reporter))
+
+          (it "Prints 1..5 as tap description"
+              (let [description (generate-description-with-x-charcteristics 5)]
+                (should= "1..5\n" 
+                         (with-out-str 
+                           (.report-description @reporter description))))))
