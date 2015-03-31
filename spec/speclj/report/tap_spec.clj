50c9e841c8130ffba579ccb8a6f0b72d0da96ef2
Juha Heimonen <heimojuh@gmail.com>
2015-03-31 16:53:51 +0300

    Adds test for test reporting ok line
    Verifies that ok line is printed correctly

--- spec/speclj/report/tap_spec.clj
+++ spec/speclj/report/tap_spec.clj
@@ -1,6 +1,9 @@
 (ns speclj-tap-runner.core-spec
   (:require [speclj.core :refer :all]
-            [speclj.report.tap :as tap])
+            [speclj.results :as results]
+            [speclj.report.tap :as tap]
+            [clojure.pprint :as pprint]
+            [speclj.components :as components])
   (:import  [speclj.components Characteristic Description]))
 
 (defn- make-characteristics [x]
@@ -10,26 +13,43 @@
   (let [characteristics (make-characteristics x)]
     (Description. "foo" "" "" "" characteristics "" "" "" "" "" "" "" "" "")))
 
-(describe "Tap Plan is printed correctly"
+(defn- generate-test-result [name]
+  (let [characteristic (components/->Characteristic name "" "")]
+    (results/->PassResult characteristic 0)))
+
+(describe "Tap Reporter tests"
 
           (around [it]
                   (with-out-str (it)))
 
           (with reporter (tap/new-tap-reporter))
 
-          (it "Prints 1..5 as tap description"
-              (let [description (generate-description-with-x-charcteristics 5)]
-                (should= "1..5\n" 
-                         (with-out-str 
-                           (.report-description @reporter description)))))
-          
-          (it "Prints 1..0 as tap description. Library we use always starts from 1"
-              (let [description (generate-description-with-x-charcteristics 0)]
-                (should= "1..0\n"
-                         (with-out-str
-                           (.report-description @reporter description)))))
-          (it "Prints 1..1000 as tap description"
-              (let [description (generate-description-with-x-charcteristics 1000)]
-                (should= "1..1000\n"
-                         (with-out-str
-                           (.report-description @reporter description))))))
+          (describe "Tap Plan tests"
+
+                    (it "Prints 1..5 as tap description"
+                        (let [description (generate-description-with-x-charcteristics 5)]
+                          (should= "1..5\n" 
+                                   (with-out-str 
+                                     (.report-description @reporter description)))))
+
+                    (it "Prints 1..0 as tap description. Library we use always starts from 1"
+                        (let [description (generate-description-with-x-charcteristics 0)]
+                          (should= "1..0\n"
+                                   (with-out-str
+                                     (.report-description @reporter description)))))
+
+                    (it "Prints 1..1000 as tap description"
+                        (let [description (generate-description-with-x-charcteristics 1000)]
+                          (should= "1..1000\n"
+                                   (with-out-str
+                                     (.report-description @reporter description))))))
+
+          (describe "Testline tests"
+
+                    (it "Prints ok testcase 1 when testcase 1 is successful"
+                        (let [testcaseresult (generate-test-result "testcase 1")]
+                          (pprint/pprint testcaseresult)
+                          (should= "ok testcase 1\n"
+                                   (with-out-str
+                                     (.report-pass @reporter testcaseresult)))))))
+
