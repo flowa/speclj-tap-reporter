387a8ddccc6a7dc52d07e2163c39bb70513e6cdb
Juha Heimonen <heimojuh@gmail.com>
2015-04-01 19:10:17 +0300

    Still not working and I wonder why

--- spec/speclj/report/tap_spec.clj
+++ spec/speclj/report/tap_spec.clj
@@ -18,8 +18,8 @@
     (results/->PassResult characteristic 0)))
 
 (defn- get-last-line []
- (let [l tap/lines] 
-  (last @l)))
+  (let [l tap/lines] 
+    (last @l)))
 
 (defn- assert-report-description-simple [description reporter result]
   (.report-description reporter description)
@@ -28,35 +28,41 @@
 
 (describe "Tap Reporter tests"
 
-
           (with reporter (tap/new-tap-reporter))
 
-          (describe "Tap Plan tests"
-          (around [it]
-                  (with-out-str (it)))
-          (after (tap/reset))
+            (describe "Tap Plan tests"
+                      (after (tap/reset))
 
-                    (it "Prints 1..5 as tap description"
-                        (let [description (generate-description-with-x-charcteristics 5)]
-                          (assert-report-description-simple description @reporter "1..5")))
+                      (around [it]
+                              (with-redefs [tap/write-to-file (fn [foo] nil)
+                                            tap/lines (atom [])] (it)))
 
-                    (it "Prints 1..0 as tap description. Library we use always starts from 1"
-                        (let [description (generate-description-with-x-charcteristics 0)]
-                          (assert-report-description-simple description @reporter "1..0")))
+                      (it "Prints 1..5 as tap description"
+                          (let [description (generate-description-with-x-charcteristics 5)]
+                            (assert-report-description-simple description @reporter "1..5")))
 
-                    (it "Prints 1..1000 as tap description"
-                        (let [description (generate-description-with-x-charcteristics 1000)]
-                          (assert-report-description-simple description @reporter "1..1000"))))
+                      (it "Prints 1..0 as tap description. Library we use always starts from 1"
+                          (let [description (generate-description-with-x-charcteristics 0)]
+                            (assert-report-description-simple description @reporter "1..0")))
 
-          (describe "Testline tests"
+                      (it "Prints 1..1000 as tap description"
+                          (let [description (generate-description-with-x-charcteristics 1000)]
+                            (assert-report-description-simple description @reporter "1..1000"))))
 
-                    (it "Prints ok testcase 1 when testcase 1 is successful"
-                        (let [testcaseresult (generate-test-result "testcase 1")]
-                          (.report-pass @reporter testcaseresult)    
-                          (should= "ok testcase 1" (get-last-line))))
+            (describe "Testline tests"
+                      (after (tap/reset))
 
-                    (it "Prints not ok testcase 1 when testcase 1 is not successfull"
-                        (let [testcaseresult (generate-test-result "testcase 1")]
-                          (.report-fail @reporter testcaseresult)
-                          (should= "not ok testcase 1" (get-last-line))))))
+                      (around [it]
+                              (with-redefs [tap/write-to-file (fn [foo] nil)
+                                            tap/lines (atom [])] (it)))
+
+                      (it "Prints ok testcase 1 when testcase 1 is successful"
+                          (let [testcaseresult (generate-test-result "testcase 1")]
+                            (.report-pass @reporter testcaseresult)    
+                            (should= "ok testcase 1" (get-last-line))))
+
+                      (it "Prints not ok testcase 1 when testcase 1 is not successfull"
+                          (let [testcaseresult (generate-test-result "testcase 1")]
+                            (.report-fail @reporter testcaseresult)
+                            (should= "not ok testcase 1" (get-last-line))))))
 
--- src/speclj/report/tap.clj
+++ src/speclj/report/tap.clj
@@ -1,5 +1,6 @@
 (ns speclj.report.tap
   (:require [speclj.reporting]
+            [clojure.pprint :as pprint]
             [clojure.java.io :as io]))
 
 (def tests-total (atom 0))
@@ -7,6 +8,10 @@
 (def lines (atom []))
 
 (def tap-result-file "./target/tap-result.tap")
+
+(defn write-to-file [results]
+  (with-open [writer (io/writer tap-result-file)]
+    (doseq [line @lines] (.write writer (str line "\n")))))
 
 (defn- log [stuff]
   (swap! lines conj stuff))
@@ -16,14 +21,14 @@
 
 (defn- get-number-of-tests-from-description [description]
   (let [descriptions (.charcteristics description)]
-  (count @descriptions)))
+    (count @descriptions)))
 
 (deftype TapReporter [passes fails results]
   speclj.reporting/Reporter
   (report-message [this message])
   (report-description [this description]
     (let [tests-in-current-plan (get-number-of-tests-from-description description)]
-    (swap! tests-total #(+ % tests-in-current-plan))))
+      (swap! tests-total #(+ % tests-in-current-plan))))
   (report-pass [this result]
     (let [msg (get-name-from-result result)]
       (log (str "ok " msg))))
@@ -31,12 +36,14 @@
   (report-fail [this fail]
     (let [msg (get-name-from-result fail)]
       (log (str "not ok " msg))))
-  (report-runs [this runs]
-    (log (str "1.." @tests-total)))
+  (report-runs [this results]
+    (log (str "1.." @tests-total))
+    (write-to-file @lines))
   (report-error [this error]))
 
 (defn reset []
-    (reset! tests-total 0)
-    (reset! lines []) )
+  (reset! tests-total 0)
+  (reset! lines []))
+
 (defn new-tap-reporter []
   (TapReporter. 0 0 0))
